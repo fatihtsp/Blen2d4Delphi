fe5581e124a1da0849c108570eb77cbe222fb2f6
Fatih Taspınar <fatihtsp@gmail.com>
2023-01-15 03:56:09 +0300

    Add files via upload

---
+++ Samples/FMX/Particles/blFmxParticles.dpr
@@ -0,0 +1,21 @@
+program blFmxParticles;
+
+uses
+  FastMM5,
+  System.StartUpCopy,
+  FMX.Forms,
+  FMain in 'FMain.pas' {FormMain},
+  FBase in '..\Common\FBase.pas' {FormBase},
+  Blend2d.Api,
+  Blend2D in '..\..\..\Blend2D.pas';
+
+{$R *.res}
+
+begin
+  ReportMemoryLeaksOnShutdown := true;
+  IsMultiThread := True;
+  blRuntimeInit();
+  Application.Initialize;
+  Application.CreateForm(TFormMain, FormMain);
+  Application.Run;
+end.
